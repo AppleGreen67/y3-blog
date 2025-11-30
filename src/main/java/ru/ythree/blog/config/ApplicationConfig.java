bb002207bc1c08f80dda1ac617cca1273e08bc71
applegreen <igreenapple67@gmail.com>
2025-11-30 22:52:11 +0500

    Запуск на tomcat+docker

---
+++ src/main/java/ru/ythree/blog/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package ru.ythree.blog.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.ythree.blog")
+public class ApplicationConfig {
+
+}
