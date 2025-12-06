ab1a6f59dfb5533572c2c0bb42230402501bbf9e
applegreen <igreenapple67@gmail.com>
2025-12-06 11:30:00 +0500

    cors конфиг

--- src/main/java/ru/ythree/blog/config/ApplicationConfig.java
+++ src/main/java/ru/ythree/blog/config/ApplicationConfig.java
@@ -1,15 +1,29 @@
 package ru.ythree.blog.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.ythree.blog")
 @PropertySource("classpath:application.properties")
 public class ApplicationConfig {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
